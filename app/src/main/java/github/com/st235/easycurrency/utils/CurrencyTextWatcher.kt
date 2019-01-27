9b0e51bf4e93b203369bb664c091c1829848085e
Alexander Dadukin <st235@yandex.ru>
2019-01-27 19:24:30 +0300

    numer formatter exception fixed.

--- app/src/main/java/github/com/st235/easycurrency/utils/CurrencyTextWatcher.kt
+++ app/src/main/java/github/com/st235/easycurrency/utils/CurrencyTextWatcher.kt
@@ -2,12 +2,22 @@ package github.com.st235.easycurrency.utils
 
 import android.text.Editable
 import android.text.TextWatcher
+import java.text.NumberFormat
+import java.util.*
 
 abstract class CurrencyTextWatcher: TextWatcher {
 
     override fun afterTextChanged(s: Editable?) {
+        val format = NumberFormat.getInstance(Locale.getDefault())
+        var result = 0.0
         val text = s.toString()
-        onValueChanged(if (text.isEmpty()) 0.0 else text.toDouble())
+        if (text.isNotEmpty()) {
+            val number = format.parse(s.toString())
+            result = number.toDouble()
+        }
+
+
+        onValueChanged(result)
     }
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
